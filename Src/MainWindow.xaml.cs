6459220ec44b611344564f03456c67edba60a803
靳兆鲁 <1756404649@qq.com>
2020-05-28 22:46:36 +0800

    add LoginWindow

---
+++ Src/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using FishMovie.Core.Windows;
+using System.Windows;
+
+namespace FishMovie.Windows
+{
+    ///<summary>
+    ///**************************************************
+    /// Version：v1.0.0
+    /// Author：NESP_Technology-JinZhaolu
+    /// Created Date：2020/5/28 20:45:52
+    /// Description：
+    ///****************************************************
+    ///</summary> 
+    public partial class LoginWindow : BaseWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            //LoginWindow startup on center of screen
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+    }
+}
